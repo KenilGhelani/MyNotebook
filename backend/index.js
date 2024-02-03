4c725e61355cb99b675c18676333e3432ff51011
KenilGhelani <kenilghelani2004@gmail.com>
2024-02-03 14:05:42 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,24 @@
 const connectToMongo = require("./db");
 const express = require("express");
-var cors = require('cors');
+var cors = require("cors");
 
 connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mynotebook-frontend.vercel.app",
+  })
+);
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Kenil Ghelani!') 
-})
+app.get("/", (req, res) => {
+  res.send("Hello Kenil Ghelani!");
+});
 
-app.use('/api/auth', require('./Routes/Auth'));
-app.use('/api/notes', require('./Routes/Notes'));
+app.use("/api/auth", require("./Routes/Auth"));
+app.use("/api/notes", require("./Routes/Notes"));
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`);
